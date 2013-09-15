45ae56342bb7165522a62d66cb7c451a98c0d8ef
Tony-Liang <liangsoft28@163.com>
2013-09-15 21:23:01 +0800

    Signed-off-by: Tony-Liang <liangsoft28@163.com>

--- Web/StringUtility.cs
+++ Web/StringUtility.cs
@@ -458,6 +458,69 @@ namespace LCW.Framework.Common.Web
 
         #endregion
 
+        public static string GetSubString(string str, int length, bool flag)
+        {
+            string temp = str;
+            int j = 0;
+            int k = 0;
+            for (int i = 0; i < temp.Length; i++)
+            {
+                if (Regex.IsMatch(temp.Substring(i, 1), @"[\u4e00-\u9fa5]+"))
+                {
+                    j += 2;
+                }
+                else
+                {
+                    j += 1;
+                }
+                if (j <= length)
+                {
+                    k += 1;
+                }
+                if (j >= length)
+                {
+                    temp = temp.Substring(0, k);
+                    break;
+                }
+            }
+            if (flag)
+            {
+                temp += "...";
+            }
+            return temp;
+        }
+
+        public static string GetSubStringD(string str, int length, bool flag)
+        {
+            string temp = str;
+            int j = 0;
+            int k = 0;
+            for (int i = 0; i < temp.Length; i++)
+            {
+                if (temp[i] < 27 || temp[i] > 126)
+                {
+                    j += 2;
+                }
+                else
+                {
+                    j += 1;
+                }
+                if (j <= length)
+                {
+                    k += 1;
+                }
+                if (j >= length)
+                {
+                    temp = temp.Substring(0, k);
+                    break;
+                }
+            }
+            if (flag)
+            {
+                temp += "...";
+            }
+            return temp;
+        }
 
     }
 }
